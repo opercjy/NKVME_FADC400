655d53157d9625fe1b977ec46eeb148079528f40
Jiyoung CHOI <158591410+opercjy@users.noreply.github.com>
2026-03-21 18:07:11 +0900

    Update FadcBD.cc

--- objects/src/FadcBD.cc
+++ objects/src/FadcBD.cc
@@ -28,6 +28,16 @@ void FadcBD::InitDefaults() {
 }
 
 void FadcBD::Print(Option_t * /*option*/) const {
-    std::cout << Form(" FADC [mid=%d] NCH:%d NDP:%d RST:%d TLT:0x%X TOW:%d DCE:%d", 
-                      _mid, _nch, _ndp, _rst, _tlt, _tow, _dce) << std::endl;
+    std::cout << "==============================================================" << std::endl;
+    std::cout << Form(" FADC Board [MID = %d] Global Settings", _mid) << std::endl;
+    std::cout << Form("  - NCH: %d | NDP: %d | RST: %d | TLT: 0x%04X | TOW: %d | DCE: %d", 
+                      _nch, _ndp, _rst, _tlt, _tow, _dce) << std::endl;
+    std::cout << "--------------------------------------------------------------" << std::endl;
+    std::cout << " CH |  THR  |  DLY  | DACOFF | DACGAIN | POL | CW  | TM | DT  " << std::endl;
+    std::cout << "--------------------------------------------------------------" << std::endl;
+    for(int i=0; i<_nch; i++) {
+        std::cout << Form(" %2d | %5d | %5d | %6d | %7d | %3d | %3d | %2d | %2d ",
+                          i, _thr[i], _dly[i], _dacoff[i], _dacgain[i], _pol[i], _cw[i], _tm[i], _dt[i]) << std::endl;
+    }
+    std::cout << "==============================================================" << std::endl;
 }
